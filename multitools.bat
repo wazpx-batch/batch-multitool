6794ada1eb245767c21683c9d88d036ef8bb9bd4
wazpx <wishdimitri@gmail.com>
2024-09-12 19:57:06 -0400

    Add files via upload

---
+++ multitools.bat
@@ -0,0 +1,303 @@
+@echo off
+chcp 65001
+cls 
+title loading...
+
+:DISPLAY1
+
+echo                  sJJJ~^..                                             
+echo                 .GGPGGP55J7!^s..                                      
+echo                 ^#BBGGGPPP55P55YJ7!^s..                               
+echo                 ?###BBBBBBBGGPPPPPPPP5YJ7~^s..                        
+echo                 .^~JPGB##dsGGGGGBBGGPPPPP555YJ?7~^s.                  
+echo                      ..^!JPGBBBBBBB#GGBGGPP555PPPPP5YJ7J!.            
+echo                            .5#GYPGB#BGGGGGGGPGGGGGGPPPPPPP.           
+echo                            sG!    .?KS#BBGGBBBPPPGGGGGGGG!            
+echo                           sGB^     5PPGH#BGGBBBBGPGB####B.            
+echo                           .^!???7P5!s^YOPBGPPPPPGGVSG~SGH             
+echo                                .s^~!!!?#P#GGBBBGGBS#.                 
+echo                                        JF#BGGGGBGB7P.                 
+echo                                        PH#BBGGGGGB#Bs                 
+echo                                        SB#BBBBGBGBB#Y                 
+echo                                        !N#BBGBB##BB##s                
+echo                                        .G#BBBBB###BB8Y                
+echo                                        .B#BBBBBBBBBHGB                
+echo                                         1BGBBB###BGGG?                
+echo                                           265K^~7?YG
+echo.
+
+
+echo  ___                        __                          
+echo /\_ \                      /\ \  __                     
+echo \//\ \     ___      __     \_\ \/\_\    ___      __     
+echo   \ \ \   / __`\  /'__`\   /'_` \/\ \ /' _ `\  /'_ `\   
+echo    \_\ \_/\ \L\ \/\ \L\.\_/\ \L\ \ \ \/\ \/\ \/\ \L\ \  
+echo    /\____\ \____/\ \__/.\_\ \___,_\ \_\ \_\ \_\ \____ \ 
+echo    \/____/\/___/  \/__/\/_/\/__,_ /\/_/\/_/\/_/\/___L\ \
+echo                                                  /\____/
+echo                                                  \_/__/ 
+
+
+timeout /t 1 /nobreak >nul
+cls
+
+
+echo                                              ..^~JJJs                  
+echo                                       ..s^!7J55PGGPGG.                 
+echo                                ..s^!7JY55P55PPPGGGBB#^                 
+echo                         ..s^~7JY5PPPPPPPPGGBBBBBBB###?                 
+echo                   .s^~7?JY555PPPPPGGBBGGGGGsd##BGPJ~^.                 
+echo             .!J7JY5PPPPP555PPGGBGG#BBBBBBBGPJ!^..                      
+echo            .PPPPPPPGGGGGGPGGGGGGGB#BGPYG#5.                            
+echo             !GGGGGGGGPPPBBBGGBB#SK?.    !Gs                            
+echo             .B####BGPGBBBBGGB#HGPP5     ^BGs                           
+echo              HGS~GSVGGPPPPPGBPOY^s!5P7???!^.                           
+echo                  .#SBGGBBBGG#P#?!!!~^s.                                
+echo                  .P7BGBGGGGB#FJ                                        
+echo                  sB#BGGGGGBB#HP                                        
+echo                  Y#BBGBGBBBB#BS                                        
+echo                 s##BB##BBGBB#N!                                        
+echo                 Y8BB###BBBBB#G.                                        
+echo                 BGHBBBBBBBBB#B.                                        
+echo                 ?GGGB###BBBGB1                                         
+echo                   GY?7~^K562                                           
+echo.
+
+echo  ___                        __                             
+echo /\_ \                      /\ \  __                        
+echo \//\ \     ___      __     \_\ \/\_\    ___      __        
+echo   \ \ \   / __`\  /'__`\   /'_` \/\ \ /' _ `\  /'_ `\      
+echo    \_\ \_/\ \L\ \/\ \L\.\_/\ \L\ \ \ \/\ \/\ \/\ \L\ \  __ 
+echo    /\____\ \____/\ \__/.\_\ \___,_\ \_\ \_\ \_\ \____ \/\_\
+echo    \/____/\/___/  \/__/\/_/\/__,_ /\/_/\/_/\/_/\/___L\ \/_/
+echo                                                  /\____/   
+echo                                                  \_/__/    
+
+
+timeout /t 1 /nobreak >nul
+cls
+
+
+echo                  sJJJ~^..                                             
+echo                 .GGPGGP55J7!^s..                                      
+echo                 ^#BBGGGPPP55P55YJ7!^s..                               
+echo                 ?###BBBBBBBGGPPPPPPPP5YJ7~^s..                        
+echo                 .^~JPGB##dsGGGGGBBGGPPPPP555YJ?7~^s.                  
+echo                      ..^!JPGBBBBBBB#GGBGGPP555PPPPP5YJ7J!.            
+echo                            .5#GYPGB#BGGGGGGGPGGGGGGPPPPPPP.           
+echo                            sG!    .?KS#BBGGBBBPPPGGGGGGGG!            
+echo                           sGB^     5PPGH#BGGBBBBGPGB####B.            
+echo                           .^!???7P5!s^YOPBGPPPPPGGVSG~SGH             
+echo                                .s^~!!!?#P#GGBBBGGBS#.                 
+echo                                        JF#BGGGGBGB7P.                 
+echo                                        PH#BBGGGGGB#Bs                 
+echo                                        SB#BBBBGBGBB#Y                 
+echo                                        !N#BBGBB##BB##s                
+echo                                        .G#BBBBB###BB8Y                
+echo                                        .B#BBBBBBBBBHGB                
+echo                                         1BGBBB###BGGG?                
+echo                                           265K^~7?YG
+echo.
+
+echo  ___                        __                                 
+echo /\_ \                      /\ \  __                            
+echo \//\ \     ___      __     \_\ \/\_\    ___      __            
+echo   \ \ \   / __`\  /'__`\   /'_` \/\ \ /' _ `\  /'_ `\          
+echo    \_\ \_/\ \L\ \/\ \L\.\_/\ \L\ \ \ \/\ \/\ \/\ \L\ \  __  __ 
+echo    /\____\ \____/\ \__/.\_\ \___,_\ \_\ \_\ \_\ \____ \/\_\/\_\
+echo    \/____/\/___/  \/__/\/_/\/__,_ /\/_/\/_/\/_/\/___L\ \/_/\/_/
+echo                                                  /\____/       
+echo                                                  \_/__/        
+
+
+
+timeout /t 1 /nobreak >nul
+cls
+
+
+echo                                              ..^~JJJs                  
+echo                                       ..s^!7J55PGGPGG.                 
+echo                                ..s^!7JY55P55PPPGGGBB#^                 
+echo                         ..s^~7JY5PPPPPPPPGGBBBBBBB###?                 
+echo                   .s^~7?JY555PPPPPGGBBGGGGGsd##BGPJ~^.                 
+echo             .!J7JY5PPPPP555PPGGBGG#BBBBBBBGPJ!^..                      
+echo            .PPPPPPPGGGGGGPGGGGGGGB#BGPYG#5.                            
+echo             !GGGGGGGGPPPBBBGGBB#SK?.    !Gs                            
+echo             .B####BGPGBBBBGGB#HGPP5     ^BGs                           
+echo              HGS~GSVGGPPPPPGBPOY^s!5P7???!^.                           
+echo                  .#SBGGBBBGG#P#?!!!~^s.                                
+echo                  .P7BGBGGGGB#FJ                                        
+echo                  sB#BGGGGGBB#HP                                        
+echo                  Y#BBGBGBBBB#BS                                        
+echo                 s##BB##BBGBB#N!                                        
+echo                 Y8BB###BBBBB#G.                                        
+echo                 BGHBBBBBBBBB#B.                                        
+echo                 ?GGGB###BBBGB1                                         
+echo                   GY?7~^K562                                           
+echo.
+
+echo  ___                        __                                     
+echo /\_ \                      /\ \  __                                
+echo \//\ \     ___      __     \_\ \/\_\    ___      __                
+echo   \ \ \   / __`\  /'__`\   /'_` \/\ \ /' _ `\  /'_ `\              
+echo    \_\ \_/\ \L\ \/\ \L\.\_/\ \L\ \ \ \/\ \/\ \/\ \L\ \  __  __  __ 
+echo    /\____\ \____/\ \__/.\_\ \___,_\ \_\ \_\ \_\ \____ \/\_\/\_\/\_\
+echo    \/____/\/___/  \/__/\/_/\/__,_ /\/_/\/_/\/_/\/___L\ \/_/\/_/\/_/
+echo                                                  /\____/           
+echo                                                  \_/__/            
+
+
+
+timeout /t 1 /nobreak >nul
+cls
+
+title wazpx-tool
+color D
+:wazpx-tool
+echo       ██╗    ██╗ █████╗ ███████╗██████╗ ██╗  ██╗     
+echo       ██║    ██║██╔══██╗╚══███╔╝██╔══██╗╚██╗██╔╝     
+echo █████╗██║ █╗ ██║███████║  ███╔╝ ██████╔╝ ╚███╔╝█████╗
+echo ╚════╝██║███╗██║██╔══██║ ███╔╝  ██╔═══╝  ██╔██╗╚════╝
+echo       ╚███╔███╔╝██║  ██║███████╗██║     ██╔╝ ██╗     
+echo        ╚══╝╚══╝ ╚═╝  ╚═╝╚══════╝╚═╝     ╚═╝  ╚═╝     
+echo.
+echo [97m┌────────────────────────────┐
+echo │ 1. [92mip pinger               [97m│
+echo │                            │
+echo │ 2. [92mip lookup               [97m│
+echo │                            │
+echo │ 3. [92mip grabber              [97m│
+echo └────────────────────────────┘
+
+set /p input= enter option:
+
+
+if /I "%input%"=="1" goto pinger
+if /I "%input%"=="2" goto iplookup
+if /I "%input%"=="3" goto puller
+
+pause
+goto wazpx-tool
+
+:pinger
+
+title wazpx-pinger
+mode 60,20
+color 1
+
+echo       ██████╗ ██╗███╗   ██╗ ██████╗ ███████╗██████╗       
+echo       ██╔══██╗██║████╗  ██║██╔════╝ ██╔════╝██╔══██╗      
+echo █████╗██████╔╝██║██╔██╗ ██║██║  ███╗█████╗  ██████╔╝█████╗
+echo ╚════╝██╔═══╝ ██║██║╚██╗██║██║   ██║██╔══╝  ██╔══██╗╚════╝
+echo       ██║     ██║██║ ╚████║╚██████╔╝███████╗██║  ██║      
+echo       ╚═╝     ╚═╝╚═╝  ╚═══╝ ╚═════╝ ╚══════╝╚═╝  ╚═╝
+echo===========================================================
+set /p IP=enter bums ip:
+:rainbow
+PING -n 1 %IP% | FIND "TTL="
+IF ERRORLEVEL 1 (SET in=c & echo offline...)
+ping -t 2 0 10 127.0.0.1 >nul
+
+color a
+PING -n 1 %IP% | FIND "TTL="
+IF ERRORLEVEL 1 (SET in=c & echo offline...)
+ping -t 2 0 10 127.0.0.1 >nul
+color 02
+PING -n 1 %IP% | FIND "TTL="
+IF ERRORLEVEL 1 (SET in=c & echo offline...) 
+ping -t 2 0 10 127.0.0.1 >nul
+color 03
+PING -n 1 %IP% | FIND "TTL="
+IF ERRORLEVEL 1 (SET in=c & echo offline...) 
+ping -t 2 0 10 127.0.0.1 >nul
+color 04
+PING -n 1 %IP% | FIND "TTL="
+IF ERRORLEVEL 1 (SET in=c & echo offline...)
+ping -t 2 0 10 127.0.0.1 >nul
+color 05
+PING -n 1 %IP% | FIND "TTL="
+IF ERRORLEVEL 1 (SET in=c & echo offline...) 
+ping -t 2 0 10 127.0.0.1 >nul
+color 06
+PING -n 1 %IP% | FIND "TTL="
+IF ERRORLEVEL 1 (SET in=c & echo offline...) 
+ping -t 2 0 10 127.0.0.1 >nul
+color 07
+PING -n 1 %IP% | FIND "TTL="
+IF ERRORLEVEL 1 (SET in=c & echo offline...) 
+ping -t 2 0 10 127.0.0.1 >nul
+color 08
+PING -n 1 %IP% | FIND "TTL="
+IF ERRORLEVEL 1 (SET in=c & echo offline...) 
+ping -t 2 0 10 127.0.0.1 >nul
+color 09
+PING -n 1 %IP% | FIND "TTL="
+IF ERRORLEVEL 1 (SET in=c & echo offline...)
+ping -t 2 0 10 127.0.0.1 >nul
+color 0a
+PING -n 1 %IP% | FIND "TTL="
+IF ERRORLEVEL 1 (SET in=c & echo offline...)
+ping -t 2 0 10 127.0.0.1 >nul
+color 0b
+PING -n 1 %IP% | FIND "TTL="
+IF ERRORLEVEL 1 (SET in=c & echo offline...) 
+ping -t 2 0 10 127.0.0.1 >nul
+color 0c
+PING -n 1 %IP% | FIND "TTL="
+IF ERRORLEVEL 1 (SET in=c & echo offline...) 
+ping -t 2 0 10 127.0.0.1 >nul
+color 0d
+PING -n 1 %IP% | FIND "TTL="
+IF ERRORLEVEL 1 (SET in=c & echo offline...)
+ping -t 2 0 10 127.0.0.1 >nul
+color 0e
+PING -n 1 %IP% | FIND "TTL="
+IF ERRORLEVEL 1 (SET in=c & echo offline...)
+ping -t 2 0 10 127.0.0.1 >nul
+color 0f
+PING -n 1 %IP% | FIND "TTL="
+IF ERRORLEVEL 1 (SET in=c & echo offline...)
+ping -t 2 0 10 127.0.0.1 >nul
+goto rainbow
+pause
+goto wazpx-tool
+
+:iplookup
+title ip-lookup
+color 4
+:menu
+cls
+echo       ██╗██████╗     ██╗      ██████╗  ██████╗ ██╗  ██╗██╗   ██╗██████╗       
+echo       ██║██╔══██╗    ██║     ██╔═══██╗██╔═══██╗██║ ██╔╝██║   ██║██╔══██╗      
+echo █████╗██║██████╔╝    ██║     ██║   ██║██║   ██║█████╔╝ ██║   ██║██████╔╝█████╗
+echo ╚════╝██║██╔═══╝     ██║     ██║   ██║██║   ██║██╔═██╗ ██║   ██║██╔═══╝ ╚════╝
+echo       ██║██║         ███████╗╚██████╔╝╚██████╔╝██║  ██╗╚██████╔╝██║           
+echo       ╚═╝╚═╝         ╚══════╝ ╚═════╝  ╚═════╝ ╚═╝  ╚═╝ ╚═════╝ ╚═╝           
+                                                                              
+echo.===============================================================================
+echo enter ip for lookup:
+set /p ip=
+
+curl "https://ipinfo.io/%ip%/json" -o geolocation.json
+echo ==================================
+echo geolocation.json for %ip% added to desktop
+echo =================================
+pause
+
+:puller
+title puller-maker
+color 2
+:menu
+cls
+echo       ██╗██████╗     ██████╗ ██╗   ██╗██╗     ██╗     ███████╗██████╗       
+echo       ██║██╔══██╗    ██╔══██╗██║   ██║██║     ██║     ██╔════╝██╔══██╗      
+echo █████╗██║██████╔╝    ██████╔╝██║   ██║██║     ██║     █████╗  ██████╔╝█████╗
+echo ╚════╝██║██╔═══╝     ██╔═══╝ ██║   ██║██║     ██║     ██╔══╝  ██╔══██╗╚════╝
+echo       ██║██║         ██║     ╚██████╔╝███████╗███████╗███████╗██║  ██║      
+echo       ╚═╝╚═╝         ╚═╝      ╚═════╝ ╚══════╝╚══════╝╚══════╝╚═╝  ╚═╝      
+echo.============================================================================                                                                                                                                                                                                                                                 
+pause
+
+
+
+
